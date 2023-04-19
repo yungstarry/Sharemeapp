46ef571ee502cb4899ce4b3054ed8cb87847fc1f
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-04-19 11:35:43 +0100

    Commit message

---
+++ src/assets/index.js
@@ -0,0 +1,2 @@
+export { default as imglogo } from './logo.png';
+
---
+++ src/components/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+import { client } from "../client";
+import { gapi } from "gapi-script";
+
+const Login = () => {
+  const clientId =
+    "103545892884-nh5vud3abr1utmf6nf2fmejabtg4aj8n.apps.googleusercontent.com";
+  useEffect(() => {
+    gapi.load("client:auth2", () => {
+      gapi.auth2.init({ clientId: clientId });
+    });
+  }, []);
+
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    console.log(response);
+
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className=" relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={clientId}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
